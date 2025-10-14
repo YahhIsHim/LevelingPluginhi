7ae013da890c771055237375bec6ac6aadbf5889
YahhIsHim <akavoid28@gmail.com>
2025-10-14 23:58:40 +0100

    Create Ability3Command.java

---
+++ src/main/java/com/levelingsmp/commands/Ability3Command.java
@@ -0,0 +1,28 @@
+package com.levelingsmp.commands;
+
+import com.levelingsmp.LevelingPluginSMP;
+import com.levelingsmp.utils.StatAbilityManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Ability3Command implements CommandExecutor {
+    private final LevelingPluginSMP plugin;
+    private final StatAbilityManager manager;
+
+    public Ability3Command(LevelingPluginSMP plugin, StatAbilityManager manager) {
+        this.plugin = plugin;
+        this.manager = manager;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player p)) {
+            sender.sendMessage("Only players.");
+            return true;
+        }
+        manager.activateVitalityAbility(p);
+        return true;
+    }
+}
