8404aa4bad95bdb2160001e595c45d434e411345
YahhIsHim <akavoid28@gmail.com>
2025-10-14 23:58:00 +0100

    Create Ability1Command.java

---
+++ src/main/java/com/levelingsmp/commands/Ability1Command.java
@@ -0,0 +1,30 @@
+package com.levelingsmp.commands;
+
+import com.levelingsmp.LevelingPluginSMP;
+import com.levelingsmp.utils.StatAbilityManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+
+public class Ability1Command implements CommandExecutor {
+
+    private final LevelingPluginSMP plugin;
+    private final StatAbilityManager manager;
+
+    public Ability1Command(LevelingPluginSMP plugin, StatAbilityManager manager) {
+        this.plugin = plugin;
+        this.manager = manager;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player p)) {
+            sender.sendMessage("Only players.");
+            return true;
+        }
+        manager.activateStrengthAbility(p);
+        return true;
+    }
+}
