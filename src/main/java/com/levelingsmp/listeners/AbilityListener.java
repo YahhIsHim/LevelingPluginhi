c48bf6d7560fa45c3fcef82c1163d25dfb92becb
YahhIsHim <akavoid28@gmail.com>
2025-10-15 14:10:37 +0100

    Update AbilityListener.java

--- src/main/java/com/levelingsmp/listeners/AbilityListener.java
+++ src/main/java/com/levelingsmp/listeners/AbilityListener.java
@@ -5,19 +5,19 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.inventory.ItemStack;
 
 public class AbilityListener implements Listener {
+    private final WeaponAbilityManager abilityManager;
+
+    public AbilityListener(WeaponAbilityManager abilityManager) {
+        this.abilityManager = abilityManager;
+    }
 
     @EventHandler
-    public void onAbilityActivate(PlayerInteractEvent event) {
-        if (event.getHand() != EquipmentSlot.HAND) return;
-
+    public void onAbilityUse(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-        ItemStack item = player.getInventory().getItemInMainHand();
-        if (item == null) return;
+        if (player.getInventory().getItemInMainHand() == null) return;
 
-        WeaponAbilityManager.tryActivateAbility(player, item);
+        abilityManager.tryActivateAbility(player, player.getInventory().getItemInMainHand());
     }
 }
