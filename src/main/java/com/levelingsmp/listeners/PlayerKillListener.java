d9f82da11f59091a1f73855f4dd5c653f3b9c847
YahhIsHim <akavoid28@gmail.com>
2025-10-15 14:09:11 +0100

    Update PlayerKillListener.java

--- src/main/java/com/levelingsmp/listeners/PlayerKillListener.java
+++ src/main/java/com/levelingsmp/listeners/PlayerKillListener.java
@@ -1,47 +1,32 @@
 package com.levelingsmp.listeners;
 
-import com.levelingsmp.LevelingPluginSMP;
-import com.levelingsmp.utils.ItemFactory;
-import org.bukkit.Bukkit;
+import com.levelingsmp.managers.StatManager;
+import com.levelingsmp.utils.ItemManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.List;
-import java.util.Random;
-
 public class PlayerKillListener implements Listener {
+    private final StatManager statManager;
 
-    private final Random random = new Random();
+    public PlayerKillListener(StatManager statManager) {
+        this.statManager = statManager;
+    }
 
     @EventHandler
     public void onPlayerKill(PlayerDeathEvent event) {
+        Player killer = event.getEntity().getKiller();
+        if (killer == null) return;
+
+        statManager.addSkillPoint(killer, 1);
+
+        ItemStack point = ItemManager.createStatPoint();
+        killer.getInventory().addItem(point);
+        killer.sendMessage("§aYou gained a Skill Point!");
+
         Player victim = event.getEntity();
-        Player killer = victim.getKiller();
-
-        if (killer != null && killer != victim) {
-            // Add 1 stat point to killer
-            ItemStack point = ItemFactory.createStatPoint();
-            killer.getInventory().addItem(point);
-            killer.sendMessage("§aYou gained 1 Skill Point for killing " + victim.getName() + "!");
-
-            // Handle victim losing 1 point (if they have any)
-            List<ItemStack> inv = List.of(victim.getInventory().getContents());
-            boolean removed = false;
-            for (ItemStack item : inv) {
-                if (item != null && ItemFactory.isStatPoint(item)) {
-                    victim.getInventory().remove(item);
-                    removed = true;
-                    break;
-                }
-            }
-
-            if (removed) {
-                victim.sendMessage("§cYou lost 1 Skill Point!");
-                victim.getWorld().dropItemNaturally(victim.getLocation(), ItemFactory.createStatPoint());
-            }
-        }
+        statManager.addSkillPoint(victim, -1);
     }
 }
